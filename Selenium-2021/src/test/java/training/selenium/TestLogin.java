e9cbda0fea42372b1bfff14bd21de4a6539345fa
ntaranko <ntaranko@gmail.com>
2021-10-28 21:10:22 +0300

    Test login

---
+++ Selenium-2021/src/test/java/training/selenium/TestLogin.java
@@ -0,0 +1,39 @@
+package training.selenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class TestLogin {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("start-maximized");
+        driver = new ChromeDriver(chromeOptions);
+        wait = new WebDriverWait(driver, 10);
+
+    }
+
+
+    @Test
+    public void myTestLogin() {
+
+        driver.navigate().to(" http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
